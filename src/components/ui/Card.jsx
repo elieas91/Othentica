3999af363c915b9f770424caa91ebecd4f6d7052
michel <michel.jahshan@gmail.com>
2025-08-26 11:08:57 +0300

    Card (used in the services section) now has a transparent background by default. On hover, it shows white background with shadow

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React from 'react';
 
-const Card = ({ children, className = "" }) => {
+const Card = ({ children, className = '' }) => {
   return (
     <div
-      className={`bg-white dark:bg-gray-800 rounded-2xl p-6 shadow-professional hover:shadow-xl-professional transition-all duration-300 flex flex-col ${className}`}
+      className={`rounded-2xl p-6 transition-all duration-300 flex flex-col
+  hover:bg-white hover:dark:bg-gray-800 hover:shadow-professional hover:shadow-xl-professional ${className}`}
     >
       {children}
     </div>
