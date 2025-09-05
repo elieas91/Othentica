cacda5f2de132fb168dd4b1cdc7e3db694c7660e
Michel <michel.jahshan@gmail.com>
2025-09-05 13:27:20 +0300

    Changed the picture for the Philosophy section

--- src/components/sections/Philosophy.jsx
+++ src/components/sections/Philosophy.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Button from '../ui/Button';
-import PhilosophyImg from '../../assets/img/philosophy/philo-bg.webp';
+import OthenticaImg from '../../assets/img/philosophy/othentica-bg.webp';
 import { Link } from 'react-router-dom';
 
 const Philosophy = () => {
   return (
-    <section className="py-16 px-8 lg:px-16 bg-white dark:bg-primary" id="philosophy">
+    <section
+      className="py-16 px-8 lg:px-16 bg-white dark:bg-primary"
+      id="philosophy"
+    >
       <div className="max-w-7xl mx-auto grid lg:grid-cols-2 gap-20 items-center">
         {/* Left Image */}
         <div className="relative">
           <div className="aspect-square bg-gradient-to-br from-pink-200 to-pink-300 dark:from-pink-800 dark:to-pink-900 rounded-3xl overflow-hidden shadow-xl-professional">
             <img
-              src={PhilosophyImg}
+              src={OthenticaImg}
               alt="Philosophy"
               className="w-full h-full object-cover"
               loading="lazy"
@@ -26,10 +29,17 @@ const Philosophy = () => {
               What is Othentica?
             </h2>
             <p className="text-lg lg:text-xl text-primary dark:text-gray-200 leading-relaxed">
-            Othentica is a gamified wellness platform that helps people step into their authentic selves through science-backed, engaging daily quests. Designed as a treasure map for growth, it blends brain health, emotional resilience, and simple everyday practices into an interactive journey.
+              Othentica is a gamified wellness platform that helps people step
+              into their authentic selves through science-backed, engaging daily
+              quests. Designed as a treasure map for growth, it blends brain
+              health, emotional resilience, and simple everyday practices into
+              an interactive journey.
             </p>
             <p className="text-lg lg:text-xl text-primary dark:text-gray-200 leading-relaxed">
-            Users explore islands, unlock bridges, and grow their inner flame — gaining focus, energy, and balance along the way. Whether for individuals or organizations, Othentica makes personal growth not just achievable, but inspiring and fun.
+              Users explore islands, unlock bridges, and grow their inner flame
+              — gaining focus, energy, and balance along the way. Whether for
+              individuals or organizations, Othentica makes personal growth not
+              just achievable, but inspiring and fun.
             </p>
           </div>
           <div className="mt-8">
