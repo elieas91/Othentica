ecf325952d2352492c0ad153c13f3507cc9c3c4d
Michel <michel.jahshan@gmail.com>
2025-09-11 16:39:12 +0300

    Added a Banner hero section for the Blog page.

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-import SocialMedia from "../components/sections/Blog";
+import SocialMedia from '../components/sections/Blog';
+import Banner from '../components/ui/Banner';
+import BannerBg from '../assets/img/blog/blog_bg.webp';
 
 const Blog = () => {
   return (
-    <div className="bg-neutral dark:bg-primary py-20">
+    <div className="bg-neutral dark:bg-primary">
+      <div className="bg-blue-400/40">
+        <Banner
+          title="Ideas That Matter"
+          subtitle="Dive into in-depth blogs, fresh perspectives, and expert insights"
+          description=""
+          buttonText=""
+          buttonVariant="accent"
+          hasGradientTransparentBottom={false}
+          backgroundImage={BannerBg}
+          hasTransparentSides={false}
+          hasOverlay={true}
+        />
+      </div>
       <SocialMedia />
     </div>
   );
