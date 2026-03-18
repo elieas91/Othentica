aa0b48226f5474368402e5d42bea0f076282346c
elieas91 <asmar.elie2@gmail.com>
2026-03-19 00:52:21 +0200

    Fix formatting in SettingsManager component by removing trailing whitespace.

--- src/components/admin/SettingsManager.jsx
+++ src/components/admin/SettingsManager.jsx
@@ -5,7 +5,7 @@ import {
   CheckCircleIcon,
   XCircleIcon,
   PencilIcon
-} from '@heroicons/react/24/outline';
+} from '@heroicons/react/24/outline'; 
 import Swal from 'sweetalert2';
 import apiService from '../../services/api';
 
